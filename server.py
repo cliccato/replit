c8d360777cb491903103e8d150fbe4e2a7e834d7
iltiaaa <58629115+iltiaaa@users.noreply.github.com>
2021-07-23 21:05:35 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,25 +1,9 @@
 from flask import Flask
-
 from threading import Thread
 
-
 app = Flask('')
-
-
 @app.route('/')
 
-def home():
-
-    return "I'm alive"
-
-
-def run():
-
-  app.run(host='0.0.0.0',port=8080)
-
-
-def keep():  
-
-    t = Thread(target=run)
-
-    t.start()
+def home():    return "I'm alive"
+def run():    app.run(host='0.0.0.0',port=8080)
+def keep():    Thread(target=run).start()
